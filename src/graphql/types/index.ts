a769ff06957269a7354f1875dee4fc4c49bf16f5
Gabriel Colson <gabrielcolson@hotmail.fr>
2020-04-13 00:55:08 +0200

    add blog

--- src/graphql/types/index.ts
+++ src/graphql/types/index.ts
@@ -1,1 +1,4 @@
 export * from './Query';
+export * from './Mutation';
+export * from './User';
+export * from './Post';
---
+++ src/services/PostService.ts
@@ -0,0 +1,29 @@
+import { Post } from '@prisma/client';
+import slugify from 'slugify';
+
+import { ForbiddenError } from '../utils/errors';
+import { Service } from '../utils/Service';
+
+export class PostService extends Service {
+  async create({ title, content, authorId }: CreatePost): Promise<Post> {
+    const user = await this.db.user.findOne({ where: { id: authorId } });
+    if (!user) {
+      throw new ForbiddenError('author does not exist');
+    }
+
+    return this.db.post.create({
+      data: {
+        title,
+        content,
+        slug: slugify(title.toLowerCase()),
+        author: { connect: { id: user.id } },
+      },
+    });
+  }
+}
+
+export interface CreatePost {
+  title: string;
+  content: string;
+  authorId: string;
+}
